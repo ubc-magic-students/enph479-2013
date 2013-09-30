49b2ba1df2bbf48f20f1e920ba89ef83bb53106b
Colin Leung <colinmleung@gmail.com>
2013-09-29 17:30:47 -0700

    Add rails app template

---
+++ wotkit/web_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebApp::Application.config.secret_key_base = 'e3269112f942a345cdc1e73ea2116ed7a31a4c26361f532f221411951fdb2e2eb9885566b749fcaaad9fa3c762fac875366ba7a6404939e41437ef148e5ba2f6'
