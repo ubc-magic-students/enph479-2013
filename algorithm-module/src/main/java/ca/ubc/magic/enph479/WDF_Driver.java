c57a6f3dfc298863308385d74187727ebf60f973
Chris Yoon <chris.yoon90@gmail.com>
2013-10-09 16:22:27 -0700

    add TweetClusterJSONObject

--- algorithm-module/src/main/java/ca/ubc/magic/enph479/WDF_Driver.java
+++ algorithm-module/src/main/java/ca/ubc/magic/enph479/WDF_Driver.java
@@ -57,4 +57,8 @@ public class WDF_Driver {
 		
 		System.out.println(weather);
 	}
+	
+	
+	
+	
 }
---
+++ algorithm-module/src/main/java/ca/ubc/magic/enph479/builder/TweetClusterJSONObject.java
@@ -0,0 +1,5 @@
+package ca.ubc.magic.enph479.builder;
+
+public class TweetClusterJSONObject {
+	
+}
