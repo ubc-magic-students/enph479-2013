677afb4951bdf0c4d6ee7ebac083aa5e62bb909c
Chris Yoon <chris.yoon90@gmail.com>
2013-10-27 01:29:01 -0700

    update the crawler to receive important player stats in an arraylist

--- algorithm-module/src/main/java/ca/ubc/magic/enph479/CrawlNHL.java
+++ algorithm-module/src/main/java/ca/ubc/magic/enph479/CrawlNHL.java
@@ -2,6 +2,8 @@ package ca.ubc.magic.enph479;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.jsoup.Jsoup;
@@ -9,40 +11,87 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+/**
+ * A class to crawl http://canucks.nhl.com/club/stats.htm to get player stats.
+ * 
+ * @author chris.yoon90@gmail.com
+ *
+ */
 public class CrawlNHL {
 	
 	private final static String URL = "http://canucks.nhl.com/club/stats.htm";
 	private static Properties property = new Properties();
+	private Document doc = null;
 	
 	public CrawlNHL() throws IOException {
 		try(InputStream fileInput = getClass().getResourceAsStream("/playerIds.properties")) {
 			property.load(fileInput);
 		}
+		this.doc = Jsoup.connect(URL).get();
 	}
 	
-	public void test(String playerName) throws IOException {
+	public List<Integer> getPlayerStats(String playerName) throws IOException {
 		String[] names = playerName.split(" ");
 		String nameWithUnderScore = names[0] + "_" + names[1];
 		
-		Document doc = Jsoup.connect(URL).get();
 		Elements es = doc.select("a[href=/club/player.htm?id=" + property.getProperty(nameWithUnderScore) + "]");
 		
 		if (es.size() == 1) {
 			Element e = es.first();
-			System.out.println(e.text());
 			Element row = e.parent().parent().parent();
-			System.out.println(row.text());
-		} else if(es.size() == 0) {
-			System.err.println("Player " + playerName + " not found!");
-		}
+			//System.out.println(row.text());
+			String[] columns = row.text().split(" ");
+			//5: goals, 6: Assists, 7: Points, 8: plus/minus
+			List<Integer> performance = new ArrayList<Integer>(4);
+			performance.add(Integer.parseInt(columns[5]));
+			performance.add(Integer.parseInt(columns[6]));
+			performance.add(Integer.parseInt(columns[7]));
+			performance.add(Integer.parseInt(columns[8]));
+
+			return performance;
+			
+		} 
+		
+		System.err.println("Player " + playerName + " not found!");
+		return new ArrayList<Integer>();
+		
+	}
+	
+	public List<Double> getGoalieStats(String goalieName) throws IOException {
+		String[] names = goalieName.split(" ");
+		String nameWithUnderScore = names[0] + "_" + names[1];
+		
+		Elements es = doc.select("a[href=/club/player.htm?id=" + property.getProperty(nameWithUnderScore) + "]");
+		
+		if (es.size() == 1) {
+			Element e = es.first();
+			Element row = e.parent().parent().parent();
+			//System.out.println(row.text());
+			String[] columns = row.text().split(" ");
+			//6: goals against average, 7: wins, 8: losses, 13: save percentage
+			List<Double> performance = new ArrayList<Double>(4);
+			performance.add(Double.parseDouble(columns[6]));
+			performance.add(Double.parseDouble(columns[7]));
+			performance.add(Double.parseDouble(columns[8]));
+			performance.add(Double.parseDouble(columns[13]));
+
+			return performance;
+			
+		} 
+		
+		System.err.println("Goalie " + goalieName + " not found!");
+		return new ArrayList<Double>();
 		
 	}
 
 	public static void main(String[] args) {
 		try {
 			CrawlNHL crawler = new CrawlNHL();
-			crawler.test("Kevin Bieksa");
-		
+			for (int i : crawler.getPlayerStats("Kevin Bieksa"))
+				System.out.println(i);
+			
+			for (double d: crawler.getGoalieStats("Roberto Luongo"))
+				System.out.println(d);
 		
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
