733af5e6048e3a92ed05603d243f165a8a6768e7
Colin Leung <colinmleung@gmail.com>
2013-10-27 17:36:10 -0700

    Use UrbanOpus instead of base WoTKit

--- wotkit/tweet-bridge/app.js
+++ wotkit/tweet-bridge/app.js
@@ -13,7 +13,7 @@ var stream = T.stream('statuses/filter', { locations: vancouver });
 var command;
 stream.on('tweet', function (tweet) {
 		if (tweet && tweet.text && tweet.coordinates && tweet.coordinates.coordinates) {
-			command = "curl --user "+credentials.wotkit_access.user+":"+credentials.wotkit_access.password+" --request POST -d value="+tweet.id+" -d lng="+tweet.coordinates.coordinates[0]+" -d lat="+tweet.coordinates.coordinates[1]+" -d message=\""+tweet.text+"\" 'http://wotkit.sensetecnic.com/api/sensors/tweets-in-vancouver/data'";
+			command = "curl --user "+credentials.wotkit_access.user+":"+credentials.wotkit_access.password+" --request POST -d value="+tweet.id+" -d lng="+tweet.coordinates.coordinates[0]+" -d lat="+tweet.coordinates.coordinates[1]+" -d message=\""+tweet.text+"\" 'http://bennu.magic.ubc.ca/wotkit/api/sensors/tweets-in-vancouver/data'";
 			child = exec(command, function (error, stdout, stderr) {
 				sys.print('stdout: ' + stdout);
 				sys.print('stderr: ' + stderr);
@@ -36,41 +36,3 @@ stream.on('disconnect', function (disconnectMessage) {
 stream.on('warning', function (warning) {
     console.log("TWITTER WARNING:" + warning);
 });
-
-
-// Import Weather data to WoTKit
-var weather_command="curl --request GET 'http://api.openweathermap.org/data/2.5/weather?q=Vancouver,ca'";
-
-var input_command;
-
-setInterval(function() {
-	child = exec(weather_command, function (error, stdout, stderr) {
-		if (error !== null) {
-			console.log('exec error: ' + error);
-		}
-		var obj = JSON.parse(stdout);
-		input_command = "curl --user "+credentials.wotkit_access.user+":"+credentials.wotkit_access.password+" --request POST -d value=1 -d lat="+obj.coord.lat+" -d lng="+obj.coord.lon+" -d message=\""+stdout+"\" 'http://wotkit.sensetecnic.com/api/sensors/weather-in-vancouver/data'"
-		child = exec(input_command, function (error, stdout, stderr) {
-			/*sys.print('stdout: ' + stdout);
-			sys.print('stderr: ' + stderr);
-			if (error !== null) {
-				console.log('exec error: ' + error);
-			}*/
-		});
-	});
-}, 5000);
-
-// Export WoTKit weather data
-
-var end_time;
-var start_time;
-setInterval(function() {
-
-		end_time = new Date().getTime();
-		start_time = end_time - 10000;
-		var export_command = "curl --request GET 'http://wotkit.sensetecnic.com/api/sensors/2013enph479.weather-in-vancouver/data?start=" + start_time + "&end=" + end_time + "' >> output.txt";
-		sys.print('export_command: ' + export_command + "\n");
-		child = exec(export_command, function (error, stdout, stderr) {
-			console.log('stdout: ' + stdout);
-		});
-}, 5000);
