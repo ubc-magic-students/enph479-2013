ac3b2831a66e8eb8809fe3c774029d279f81caa1
Chris Yoon <chris.yoon90@gmail.com>
2013-11-01 12:22:52 -0700

    change clusterer

--- algorithm-module/src/main/java/ca/ubc/magic/enph479/TweetClusterer.java
+++ algorithm-module/src/main/java/ca/ubc/magic/enph479/TweetClusterer.java
@@ -1,17 +1,28 @@
 package ca.ubc.magic.enph479;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import moa.cluster.Cluster;
+import moa.cluster.Clustering;
 import moa.cluster.SphereCluster;
 import moa.clusterers.CobWeb;
+import moa.clusterers.clustree.ClusTree;
 import weka.core.Attribute;
+import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.neighboursearch.BallTree;
+import weka.core.neighboursearch.NearestNeighbourSearch;
 import ca.ubc.magic.enph479.builder.TweetCluster;
 import ca.ubc.magic.enph479.builder.TweetInstance;
 import ca.ubc.magic.enph479.builder.TwitterObject;
+import ca.ubc.magic.enph479.builder.UniqueRandomNumberGenerator;
 
 /**
  * Class for clustering tweet instances.
@@ -21,7 +32,8 @@ import ca.ubc.magic.enph479.builder.TwitterObject;
  */
 public class TweetClusterer {
 	
-	private CobWeb clusterer = new CobWeb();
+	private CobWeb cobwebClusterer = new CobWeb();
+	private ClusTree trueClusterer = new ClusTree();
 	private ArrayList<TweetCluster> tweetClusterList = new ArrayList<TweetCluster>();
 	
 	/**
@@ -40,9 +52,10 @@ public class TweetClusterer {
 	private int numAttributes = 2;
 	
 	public TweetClusterer() {
-		clusterer.prepareForUse();
-		clusterer.setCutoff(CUTOFF);
-		clusterer.setAcuity(ACUITY);
+		cobwebClusterer.prepareForUse();
+		trueClusterer.prepareForUse();
+		cobwebClusterer.setCutoff(CUTOFF);
+		cobwebClusterer.setAcuity(ACUITY);
 	}
 	
 	public TweetClusterer(int numAttributes) {
@@ -51,9 +64,10 @@ public class TweetClusterer {
 	}
 	
 	public TweetClusterer(double acuity, double cutoff) {
-		clusterer.prepareForUse();
-		clusterer.setCutoff(cutoff);
-		clusterer.setAcuity(acuity);
+		cobwebClusterer.prepareForUse();
+		trueClusterer.prepareForUse();
+		cobwebClusterer.setCutoff(cutoff);
+		cobwebClusterer.setAcuity(acuity);
 	}
 	
 	public TweetClusterer(double acuity, double cutoff, int numAttributes) {
@@ -76,18 +90,22 @@ public class TweetClusterer {
 		
 		for (TweetInstance inst : newTweets)
 			trainClusterer(inst);
-		
+
 		HashMap<Integer, ArrayList<TweetInstance>> map = new HashMap<Integer, ArrayList<TweetInstance>>();
+		Set<Integer> numLeafs = new LinkedHashSet<Integer>();
 		
 		for (Map.Entry<Integer, TwitterObject> entry : allTweets.entrySet()) {
 			TweetInstance tempTweetInst = entry.getValue().toTweetInstance(numAttributes);
 			
-			int index = returnIndex(clusterer.getVotesForInstance(tempTweetInst));
+			int index = returnIndex(cobwebClusterer.getVotesForInstance(tempTweetInst));
+
+			System.out.println();
 			
 	    	if (index == -1)
 	    		continue;
 	    	
 	    	if (map.containsKey(index)) {
+	    		numLeafs.add(index);
 	    		map.get(index).add(tempTweetInst);
 	    	} else {
 	    		ArrayList<TweetInstance> temp = new ArrayList<TweetInstance>();
@@ -95,8 +113,26 @@ public class TweetClusterer {
 	    		map.put(index, temp);
 	    	}
 		}
-
-		ArrayList<TweetCluster> tempClusterList = new ArrayList<TweetCluster>();
+		
+		int numClusters = map.size();
+		Clustering microC = trueClusterer.getMicroClusteringResult();
+		Clustering randomClustering = new Clustering();
+        UniqueRandomNumberGenerator random = new UniqueRandomNumberGenerator(numClusters);
+        for(int i = 0; i < numClusters ; i++) {
+        	randomClustering.add(microC.get(random.nextInt()));
+        }
+        
+        Clustering clustering = moa.clusterers.KMeans.gaussianMeans(randomClustering, microC);
+        System.out.println("Num Clusters: " + clustering.getClustering().size());
+        
+        for (int i = 0; i < clustering.getClustering().size(); i++) {
+            clustering.getClustering().get(i).setId(i);
+            for (double d: clustering.getClustering().get(i).getCenter())
+            	System.out.print(d + ", ");
+            System.out.println();
+        }
+         
+		/*ArrayList<TweetCluster> tempClusterList = new ArrayList<TweetCluster>();
 		double clusterId = 0.0;
 	    for (Map.Entry<Integer, ArrayList<TweetInstance>> entry : map.entrySet()) {
 	    	SphereCluster c = new SphereCluster(entry.getValue(), numAttributes);
@@ -108,14 +144,66 @@ public class TweetClusterer {
 	    	tempClusterList.add(tc);
 	    }
 	    
-	    System.out.println(clusterer.graph());
+	    System.out.println(cobwebClusterer.graph());
 	    System.out.println("Number of Clusters: " + tempClusterList.size());
 	    
-	    tweetClusterList = tempClusterList;
+	    tweetClusterList = tempClusterList;*/
+        
+        tweetClusterList = nearestNeighbour(clustering, allTweets);
 		
 		return tweetClusterList;
 		
 	}
+	
+	 private ArrayList<TweetCluster> nearestNeighbour(Clustering clustering, HashMap<Integer, TwitterObject> tweetInstanceMap) throws Exception {
+         if (tweetInstanceMap.isEmpty())
+                 return tweetClusterList;
+         
+         int numberOfClusters = clustering.getClustering().size();
+
+         ArrayList<TweetCluster> tweetClusterList = new ArrayList<TweetCluster>();
+         HashMap<List<Double>, Double> mapClusterCentersToClusterId = new HashMap<List<Double>, Double>();
+         for (Cluster c: clustering.getClustering()) {
+        	 tweetClusterList.add(new TweetCluster(c));
+        	 mapClusterCentersToClusterId.put(Collections.unmodifiableList(arrayToList(c.getCenter())), c.getId());
+         }
+
+         ArrayList<Attribute> atts = new ArrayList<Attribute>(numAttributes);
+         for (int i = 0; i < numAttributes; i++) {
+        	 atts.add(new Attribute("att"+i));
+         }
+         
+         Instances centerInstances = new Instances("ClusterCenterInstances", atts , 0);
+         for (int i = 0; i < numberOfClusters; i++) {
+        	 Instance tempInst = new DenseInstance(numAttributes);
+        	 for (int j = 0; j < atts.size(); j++) {
+        		 tempInst.setValue(atts.get(j), clustering.getClustering().get(i).getCenter()[j]);
+        	 }
+        	 centerInstances.add(tempInst);
+         }
+         
+         NearestNeighbourSearch nearestNeighbourSearch = new BallTree();
+         nearestNeighbourSearch.setInstances(centerInstances);
+         
+         for (Map.Entry<Integer, TwitterObject> entry : tweetInstanceMap.entrySet()) {
+                 Instance tempTweetInst = entry.getValue().toTweetInstance(numAttributes);
+                 Instance nearestInstance = nearestNeighbourSearch.nearestNeighbour(tempTweetInst);
+                 Double clusterId = (Double) mapClusterCentersToClusterId.get(arrayToList(nearestInstance.toDoubleArray()));
+                 tweetClusterList.get(clusterId.intValue()).addTweetMembers(entry.getKey());
+         }
+         
+         return tweetClusterList;
+
+	 }
+	 
+	 private static List<Double> arrayToList(double[] array) {
+         List<Double> list = new ArrayList<Double>();
+         for (double d : array) {
+                 list.add(d);
+         }
+         
+         return list;
+	 }
 	
 	/**
 	 * A method to train the clusterer with newTweets
@@ -135,7 +223,8 @@ public class TweetClusterer {
 		
 		Instances referenceInstances = new Instances("Dataset" + newTweets.getId(), atts , 0);
 	    newTweets.setDataset(referenceInstances);
-		clusterer.trainOnInstance(newTweets);
+		cobwebClusterer.trainOnInstance(newTweets);
+		trueClusterer.trainOnInstance(newTweets);
 		
 	}
 	
---
+++ algorithm-module/src/main/java/ca/ubc/magic/enph479/builder/UniqueRandomNumberGenerator.java
@@ -0,0 +1,31 @@
+package ca.ubc.magic.enph479.builder;
+
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+
+/**
+ * A class for generating unique random numbers.
+ * 
+ * @author chris.yoon90@gmail.com
+ *
+ */
+public class UniqueRandomNumberGenerator {
+	private final Random random = new Random();
+    private final int range;
+    Set<Integer> generated = new LinkedHashSet<Integer>();
+    
+    public UniqueRandomNumberGenerator(int range) {
+            this.range = range;
+    }
+    
+    public int nextInt() {
+            if (generated.size() == range)
+                    generated.clear();
+            while(true ) {
+                    int r = random.nextInt(range);
+                    if (generated.add(r))
+                            return r;
+            }
+    }
+}
