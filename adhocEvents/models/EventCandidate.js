07681ad172c01658adf08c6d432a393f1927995c
Chris Yoon <chris.yoon90@gmail.com>
2013-11-11 23:49:29 -0800

    add candidate center calculations

--- adhocEvents/models/EventCandidate.js
+++ adhocEvents/models/EventCandidate.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Tweet = require('./TweetObject.js');
 
 var EventCandidateSchema = mongoose.Schema({
-	//***** need to add center lat/long,
+	center: {type: [Number], index: "2dsphere"},
 	//***** need to add hash tags aggregated,
 	//***** need to add user_mentions aggregated
 	tweets: {type: [Tweet.schema], default: null},
