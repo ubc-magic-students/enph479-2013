ead850deec26fad3f399449e45d7034bd37f5753
Chris Yoon <chris.yoon90@gmail.com>
2013-11-18 13:21:19 -0800

    update in the logic for custerUpdater

--- adhocEvents/clusterUpdater.js
+++ adhocEvents/clusterUpdater.js
@@ -5,8 +5,8 @@ module.exports = function() {
 	var garbageCollectionInterval = 1000*60;
 	var candidateLifeTime = 30*60*1000; //30 minutes
 	setTimeout(function() {
-		var now = new Date().getTime()-candidateLifeTime;
-		EventCandidate.remove({updatedAt: {"$lte": new Date(now) }}, function(err) {
+		var time = new Date().getTime()-candidateLifeTime;
+		EventCandidate.remove({updatedAt: {"$lte": new Date(time) }}, function(err) {
 			if(err) {
 				console.log("err while garbage collecting for EventCadidates.");
 			} else {
@@ -18,13 +18,21 @@ module.exports = function() {
 	var tweetBelongsToEvent = function(tweet, callback) {
 		EventCandidate.find({}, function(err, results) {
 			results.forEach(function(result) {
-				if(result.center 
-					&& (helpers.calculateDistance(tweet.coordinates[1], tweet.coordinates[0]
-						, result.center[1], result.center[0]) < constants.maxDistance)
-					&& tweetContainsTheme(result.theme, tweet)) {
-					//***** append current tweet into geoRelated
-				} else if(tweetContainsTheme(result.theme, tweet)) {
-					//***** append current tweet into hashtagRelated
+				var isSameTheme = tweetContainsTheme(result.theme, tweet);
+				if(result.center) {
+					var distance = helpers.calculateDistance(tweet.coordinates[1], tweet.coordinates[0]
+						, result.center[1], result.center[0]);
+					if( (distance < constants.maxDistance) && isSameTheme === true) {
+						//***** append current tweet into geoRelated
+					} else if((distance > constants.maxDistance) && isSameTheme === true) {
+						//***** append current tweet into hashtagRelated
+					}
+					
+				} else {
+					//***** how would I calculate the center?
+					if(isSameTheme === true) {
+						//***** append current tweet into hashtagRelated
+					}
 				}
 			});
 		});
