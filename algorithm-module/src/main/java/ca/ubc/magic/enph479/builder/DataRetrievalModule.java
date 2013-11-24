7d0af8fbcd9bc3f44499849c7de7e74a8d4fc37d
Richard Lee <richard_li520@hotmail.com>
2013-11-23 19:08:48 -0800

    updated retrieval date

--- algorithm-module/src/main/java/ca/ubc/magic/enph479/builder/DataRetrievalModule.java
+++ algorithm-module/src/main/java/ca/ubc/magic/enph479/builder/DataRetrievalModule.java
@@ -22,8 +22,7 @@ public class DataRetrievalModule {
 		System.setProperty("jsse.enableSNIExtension", "false");
 
 		// date we started inputing data into bennu
-		//start_datetime = "2013 Nov 08 13:00:00";
-		start_datetime = "2013 Nov 19 13:00:00";
+		start_datetime = "2013 Nov 08 13:00:00";
 
 		// prepare for fetching
 		if (!wdf.prepareForFetching(fetch_interval, job_interval,
